5b662a51ce280fc830ce635923e010116fe70d62
HeySkidee <harshitojha46@gmail.com>
2022-06-14 00:36:33 +0530

    custom

--- background.js
+++ background.js
@@ -13,7 +13,7 @@ let blockedUrls = [
     link: "linkedin.com",
   },
   {
-    link: "web.whatsapp.com",
+    link: "discord.com",
   },
   {
     link: "reddit.com",
@@ -21,11 +21,28 @@ let blockedUrls = [
   {
     link: "pinterest.com",
   },
+  {
+    link: "open.spotify.com",
+  },
+  {
+    link: "music.apple.com",
+  },
+  {
+    link: "soundcloud.com",
+  },
+  {
+    link: "quora.com",
+  },
 ];
 
 blockedUrls.forEach((element) => {
   if (window.location.origin.includes(element.link)) {
     document.getElementsByTagName("body")[0].innerHTML =
-      '<div><img src="https://i.ibb.co/r0RScrj/banner.jpg" style="margin: 0px auto; height: 100vh; display: flex;"></div>';
+      '<div><img src="https://cdn.discordapp.com/attachments/716523839634407436/968863806522535987/Everyday_All_Day_100_2.png" style="margin: 0px auto ; height: 100vh; display: flex;"></div>';
+
+
+    // '<div><img src="https://cdn.discordapp.com/attachments/716523839634407436/968815045570101268/kankan_morning_auto_x2.jpg" style="margin: 0px auto ; height: 100vh; display: flex;"></div>';
+    // '<div><img src="https://cdn.discordapp.com/attachments/716523839634407436/968813547696054292/kankan_teddy.png" style="margin: 0px auto ; height: 100vh; display: flex;"></div>';
+
   }
 });
